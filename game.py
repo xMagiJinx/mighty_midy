0c8d27e416a0a1ff788d9a30acf9baa74bf6d1e0
John Donnal <35339269+jdonnal@users.noreply.github.com>
2022-11-03 09:49:00 -0400

    changed game behavior to modify screen background on key press/release

--- game.py
+++ game.py
@@ -1,24 +1,21 @@
 import pygame
-import time
 import sys
+
 pygame.init()
 
 screen = pygame.display.set_mode((400, 400))
 
 while True:
     # 1 check for user input (key press, mouse clicks,joystick)
-    print("---- Checking for Events! ----")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("thanks for playing")
             sys.exit()
-        # want to find out user released a key
+        # change the background if a key is
+        # pressed or released
         if event.type == pygame.KEYUP:
             screen.fill((0, 0, 255))
-        if event.type == pygame.MOUSEMOTION:
+        if event.type == pygame.KEYDOWN:
             screen.fill((255, 255, 0))
 
-    print("---- DONE Checking ----")
-
     pygame.display.flip()
-    time.sleep(1)
