bdbedb2eb52f5fae649129798b0845ae77e86473
John Donnal <35339269+jdonnal@users.noreply.github.com>
2022-11-03 09:46:43 -0400

    started my game

--- game.py
+++ game.py
@@ -1,1 +1,24 @@
-print("Might MIDY!")
+import pygame
+import time
+import sys
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+
+while True:
+    # 1 check for user input (key press, mouse clicks,joystick)
+    print("---- Checking for Events! ----")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("thanks for playing")
+            sys.exit()
+        # want to find out user released a key
+        if event.type == pygame.KEYUP:
+            screen.fill((0, 0, 255))
+        if event.type == pygame.MOUSEMOTION:
+            screen.fill((255, 255, 0))
+
+    print("---- DONE Checking ----")
+
+    pygame.display.flip()
+    time.sleep(1)
