3e7513f01173dbdee17aabeef83fd12c9a8d1fa7
John Donnal <35339269+jdonnal@users.noreply.github.com>
2022-11-04 09:46:52 -0400

    added water background

--- game.py
+++ game.py
@@ -1,21 +1,28 @@
 import pygame
 import sys
 
+TILE_SIZE = 64
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode(
+    (15*TILE_SIZE, 15*TILE_SIZE))
+print(screen.get_rect())
+water = pygame.image.load("assets/water_tile.png")
+water_rect = water.get_rect()
+screen_rect = screen.get_rect()
+
+num_tiles = screen_rect.width // water_rect.width
+print(f"We need {num_tiles} water tiles")
+
+for y in range(num_tiles):
+    for x in range(num_tiles):
+        screen.blit(water, (x*water_rect.width,y*water_rect.height))
 
 while True:
     # 1 check for user input (key press, mouse clicks,joystick)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print("thanks for playing")
             sys.exit()
-        # change the background if a key is
-        # pressed or released
-        if event.type == pygame.KEYUP:
-            screen.fill((0, 0, 255))
-        if event.type == pygame.KEYDOWN:
-            screen.fill((255, 255, 0))
+
 
     pygame.display.flip()
