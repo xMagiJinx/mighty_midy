108220a66e2a055efb4c827974538bf60840bf14
John Donnal <35339269+jdonnal@users.noreply.github.com>
2022-11-10 09:47:10 -0500

    updated island object

--- island.py
+++ island.py
@@ -1,9 +1,31 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Island:
+
+class Island(Sprite):
 
     def __init__(self):
-        self.image = pygame.image.load("assets/island_tl.png")
+        super().__init__()
+        self.image = pygame.surface.Surface((128, 128))
+        self.image.blit(pygame.image.load("assets/water_tile.png"),
+                        (0, 0))
+        self.image.blit(pygame.image.load("assets/island_tl.png"),
+                        (0, 0))
+
+        self.image.blit(pygame.image.load("assets/water_tile.png"),
+                        (64, 0))
+        self.image.blit(pygame.image.load("assets/island_tr.png"),
+                        (64, 0))
+
+        self.image.blit(pygame.image.load("assets/water_tile.png"),
+                        (0, 64))
+        self.image.blit(pygame.image.load("assets/island_bl.png"),
+                        (0, 64))
+
+        self.image.blit(pygame.image.load("assets/water_tile.png"),
+                        (64, 64))
+        self.image.blit(pygame.image.load("assets/island_br.png"),
+                        (64, 64))
         self.rect = self.image.get_rect()
 
     def move(self, coordinate):
