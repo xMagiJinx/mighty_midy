fbd9dd7f9f676fdcb4afc4c8ccb477bef2eae67b
John Donnal <35339269+jdonnal@users.noreply.github.com>
2023-04-17 08:55:49 -0400

    added pixycam logic

---
+++ pico/code.py
@@ -0,0 +1,15 @@
+from pixycam import PixyCam
+import usb_cdc
+
+camera = PixyCam()
+
+while True:
+    # wait for data from
+    usb_cdc.data.readline()
+    blocks = camera.getBlocks(1)
+    if len(blocks) == 0:
+        msg = f"-1,-1\n"
+    else:
+        block = blocks[0]
+        msg = f"{block.x},{block.y}\n"
+    usb_cdc.data.write(msg)
